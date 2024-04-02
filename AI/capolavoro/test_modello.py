7a805bd22b9241cb0f332b45f5db3d96b00011c7
RossettiMarco885 <rossettimarco885@gmail.com>
2024-04-02 22:43:02 +0200

    tornato al 55%
    non ho avuto successo con il filtro bilineare e mediano e adesso è senza

--- AI/capolavoro/test_modello.py
+++ AI/capolavoro/test_modello.py
@@ -9,21 +9,22 @@ import cv2
 
 def load_and_prepare_image(filename):
 
-     # Carica l'immagine in modalità grayscale con dimensione target di 28x28
-  img = image_utils.load_img(filename, color_mode="grayscale", target_size=(28, 28))
+    # Carica l'immagine in modalità grayscale con dimensione target di 28x28
+    img = image_utils.load_img(filename, color_mode="grayscale", target_size=(28, 28))
 
-  # Converte l'immagine in un array NumPy
-  img = image_utils.img_to_array(img)
+    # Converte l'immagine in un array NumPy
+    img = image_utils.img_to_array(img)
+    # Denoising con filtro bilaterale
+    #img = cv2.bilateralFilter(img, 7, 66, 66)
+    
+    # Reshape in un singolo campione con 1 canale
+    img = img.reshape(1, 28, 28, 1)
 
-  # Reshape in un singolo campione con 1 canale
-  img = img.reshape(1, 28, 28, 1)
-
-  # Normalizza i dati dei pixel (supponendo un intervallo di 0-255)
-  #img = 1 - img
-  img = img.astype("float32") / 255.0
-  
-
-  return img
+    # Normalizza i dati dei pixel (supponendo un intervallo di 0-255)
+    # img = 1 - img
+    img = img.astype("float32") / 255.0
+    
+    return img
 
 
 def carica_immagini(images):
@@ -42,7 +43,7 @@ def mostra_immagini(images, path_images):
     for i in range(len(images)):
 
         # Visualizza le prime 5 immagini postprocessate
-        if i < 1:
+        if i < 2:
             plt.figure()
             plt.imshow(
                 images[i].squeeze(), cmap="gray"
@@ -50,26 +51,24 @@ def mostra_immagini(images, path_images):
             plt.title(f"Immagine {i+1}")
             plt.axis("off")
             plt.show()
-   
+
 
 def predict_from_image(image):
-  """
-  Funzione per predire il numero dall'immagine.
+    """
+    Funzione per predire il numero dall'immagine.
 
-  Args:
-    image: L'immagine pre-processata in formato NumPy.
+    Args:
+      image: L'immagine pre-processata in formato NumPy.
 
-  Returns:
-    Il numero predetto dall'immagine.
-  """
-  
+    Returns:
+      Il numero predetto dall'immagine.
+    """
 
-  # Predizione del numero
-  prediction = model.predict(image)
-  numero_predetto = np.argmax(prediction)
+    # Predizione del numero
+    prediction = model.predict(image)
+    numero_predetto = np.argmax(prediction)
 
-  return numero_predetto
-
+    return numero_predetto
 
 
 # Percorso della directory delle immagini
@@ -107,22 +106,22 @@ for i in range(len(images)):
 
 
 for i in range(len(images)):
-        filename = os.listdir(path_images)[i]
-        numero_predetto=predict_from_image(images[i])
+    filename = os.listdir(path_images)[i]
+    numero_predetto = predict_from_image(images[i])
 
-        # Controllo se il numero predetto è uguale al numero nel nome del file
-        numero_nel_nome = int(filename[0])
-        if numero_nel_nome != numero_predetto:
-            predizioni_sbagliate += 1
-            print(
-                f"Errore: {filename} --> {numero_nel_nome}, il numero predetto è {numero_predetto}"
-            )
-        else:
-            predizioni_giuste += 1
-            print(
-                f"GIUSTO: {filename} --> {numero_nel_nome}, il numero predetto è {numero_predetto}"
-            )
+    # Controllo se il numero predetto è uguale al numero nel nome del file
+    numero_nel_nome = int(filename[0])
+    if numero_nel_nome != numero_predetto:
+        predizioni_sbagliate += 1
+        print(
+            f"Errore: {filename} --> {numero_nel_nome}, il numero predetto è {numero_predetto}"
+        )
+    else:
+        predizioni_giuste += 1
+        print(
+            f"GIUSTO: {filename} --> {numero_nel_nome}, il numero predetto è {numero_predetto}"
+        )
 
 print(
-        f"le predizioni giuste sono: {predizioni_giuste} \nle predizioni sbagliate sono: {predizioni_sbagliate}\nla percentuale di predizioni corrette è: {(predizioni_giuste/predizioni_sbagliate)*100}"
-    )   
+    f"le predizioni giuste sono: {predizioni_giuste} \nle predizioni sbagliate sono: {predizioni_sbagliate}\nla percentuale di predizioni corrette è: {(predizioni_giuste/predizioni_sbagliate)*100}"
+)
